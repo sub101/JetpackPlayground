762a299ffbac19caf7abaef4d759f1a170bbb128
sub101 <ssubbin10@gmail.com>
2023-11-21 23:57:15 +0900

    workManager process practice

---
+++ app/src/main/java/com/example/jetpackplayground/Workmanager/WorkmanagerProcessActivity.kt
@@ -0,0 +1,35 @@
+package com.example.jetpackplayground.Workmanager
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkInfo
+import androidx.work.WorkManager
+import com.example.jetpackplayground.R
+import com.example.jetpackplayground.Workmanager.WorkManagerTest.Companion.HowMuch
+
+class WorkmanagerProcessActivity :AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val workManagerTest = OneTimeWorkRequestBuilder<WorkManagerTest>().build()
+        WorkManager.getInstance(this).enqueue(workManagerTest)
+
+        WorkManager.getInstance(this)
+            .getWorkInfoByIdLiveData(workManagerTest.id)
+            .observe(this, Observer { workInfo : WorkInfo? ->
+                val progress = workInfo?.progress
+                val value = progress?.getInt(HowMuch, 0)
+                if(value != 0) {
+                    Log.d("MainActivity", value.toString())
+                }
+                if(value == 100) {
+                    Log.d("MainActivity", "이제 끝끝")
+                }
+            })
+
+    }
+}
