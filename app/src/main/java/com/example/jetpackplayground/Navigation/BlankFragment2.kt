edcfc7a25700842b1c565fab94d0b41da6b78a04
sub101 <ssubbin10@gmail.com>
2023-11-25 23:29:09 +0900

    practice navigation

--- app/src/main/java/com/example/jetpackplayground/Navigation/BlankFragment2.kt
+++ app/src/main/java/com/example/jetpackplayground/Navigation/BlankFragment2.kt
@@ -16,10 +16,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [BlankFragment1.newInstance] factory method to
+ * Use the [BlankFragment2.newInstance] factory method to
  * create an instance of this fragment.
  */
-class BlankFragment1 : Fragment() {
+class BlankFragment2 : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -38,10 +38,10 @@ class BlankFragment1 : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
 
-        val view = inflater.inflate(R.layout.fragment_blank1, container, false)
+        val view = inflater.inflate(R.layout.fragment_blank2, container, false)
 
-        view.findViewById<Button>(R.id.btn1).setOnClickListener {
-            Navigation.findNavController(view).navigate(R.id.action_blankFragment1_to_blankFragment2)
+        view.findViewById<Button>(R.id.btn2).setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_blankFragment2_to_blankFragment1)
         }
 
         return view
@@ -54,12 +54,12 @@ class BlankFragment1 : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment BlankFragment1.
+         * @return A new instance of fragment BlankFragment2.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            BlankFragment1().apply {
+            BlankFragment2().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
